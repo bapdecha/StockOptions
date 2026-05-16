b786751474f28255f4402bfc750701edd083f737
Baptiste de CHARRY <baptiste.de-charry@epita.fr>
2026-05-16 17:42:42 +0200

    big commit

--- src/index.py
+++ src/index.py
@@ -44,6 +44,19 @@ def calc_prime(S, K, fn, r, t, sigma):
 	d2 = d1 - sigma*sqrt(t)
 	return S*fn(norm, -100, d1) - K*exp(-r*t)*fn(norm, -100, d2), -S*fn(norm, -100, -d1) + K*exp(-r*t)*fn(norm, -100, -d2)
 
+def calc_greeks(S, K, fn, r, t, sigma):
+	d1 = (log(S/K) + (r + sigma*sigma/2)*t) / (sigma*sqrt(t))
+	d2 = d1 - sigma*sqrt(t)
+	delta_call = fn(norm, -100, d1)
+	delta_put = fn(norm, -100, d1) - 1
+	gamma = norm(d1) / (S*sigma*sqrt(t))
+	theta_call = -S * norm(d1) * sigma / (2*sqrt(t)) - r*K*exp(-r*t)*fn(norm, -100, d2)
+	theta_put = -S * norm(d1) * sigma / (2*sqrt(t)) + r*K*exp(-r*t)*fn(norm, -100, -d2)
+	rho_call = K*t*exp(-r*t)*fn(norm, -100, d2)
+	rho_put = -K*t*exp(-r*t)*fn(norm, -100, -d2)
+	vega = S*norm(d1)*sqrt(t)
+	return { 'delta_call': delta_call, 'delta_put': delta_put, 'gamma': gamma, 'theta_call': theta_call, 'theta_put': theta_put, 'rho_call': rho_call, 'rho_put': rho_put, 'vega': vega }
+
 @app.post("/simu")
 def ans():
 	action = float(request.form["action"])
@@ -58,6 +71,7 @@ def ans():
 	date = parse(request.form["terme"].split("-")) / 365
 	prime = calc_prime(action, exercice, inte, taux, date, vol)
 	values = [{ 'prix': 0, 'call': 0, 'pfc': 0, 'pfp': 0, 'put': 0 } for i in range(sup-inf+1)]
+	greeks = calc_greeks(action, exercice, inte, taux, date, vol)
 	f = open("static/csv.csv", "w")
 	for i in range(sup-inf+1):
 		values[i]['prix'] = inf+i #cours de l'action
@@ -91,7 +105,7 @@ def ans():
 				"pfp": values[i]['pfp']} for i in range(sup-inf+1)])
 	except Exception as e:
 		message = "Erreur : la collection " + name_db + " existe déjà"
-	return render_template("simu.html", values=values, exercice=exercice, action=action, prime=prime, message=message)
+	return render_template("simu.html", values=values, exercice=exercice, action=action, prime=prime, message=message, greeks=greeks)
 
 @app.get("/opt")
 def opt():
@@ -141,6 +155,91 @@ def post_db():
 	values = collection.find()
 	li = [value for value in values]
 	return render_template("db.html", values=li)
+
+@app.get("/strat")
+def get_strat():
+	return render_template("strat.html")
+
+def choose_prime(S, K, fn, r, t, sigma, strat, K2=0, K3=0, K4=0):
+	if strat == "call" or strat == "put" or strat == "straddle":
+		return calc_prime(S, K, fn, r, t, sigma)
+	elif strat == "strangle":
+		return calc_prime(S, K, fn, r, t, sigma)[1], calc_prime(S, K2, fn, r, t, sigma)[0]
+	elif strat == "butterfly":
+		return calc_prime(S, K, fn, r, t, sigma)[0], calc_prime(S, K2, fn, r, t, sigma)[0], calc_prime(S, K3, fn, r, t, sigma)[0]
+	else:
+		return calc_prime(S, K, fn, r, t, sigma)[1], calc_prime(S, K2, fn, r, t, sigma)[1], calc_prime(S, K3, fn, r, t, sigma)[0], calc_prime(S, K4, fn, r, t, sigma)[0]
+
+def curve_prime(K, fn, r, t, sigma, inf, sup):
+	values = [{ "sj": 0, "prime": 0 } for i in range(100*(sup-inf+1))]
+	for i in range(sup-inf+1):
+		values[i]["sj"] = inf+i
+		values[i]["prime"] = calc_prime(inf+i, K, fn, r, t, sigma)[0]
+	return values
+
+@app.post("/strat")
+def post_strat():
+	sj = int(request.form["sous-jacent"])
+	taille = int(request.form["taille"])
+	nb = int(request.form["nb"])
+	inf = int(request.form["inf"])
+	sup = int(request.form["sup"])
+	taux = float(request.form["taux"]) / 100
+	vol = float(request.form["vol"]) / 100
+	date = parse(request.form["terme"].split("-")) / 365
+	strat = str(request.form["type"])
+	sens = str(request.form["sens"])
+	strike1 = int(request.form["exercice"])
+	try:
+		strike2 = int(request.form["exercice2"])
+	except:
+		strike2 = None
+	try:
+		strike3 = int(request.form["exercice3"])
+	except:
+		strike3 = None
+	try:
+		strike4 = int(request.form["exercice4"])
+	except:
+		strike4 = None
+	if strike4:
+		prime = choose_prime(sj, strike1, inte, taux, date, vol, strat, K2=strike2, K3=strike3, K4=strike4)
+	elif strike3:
+		prime = choose_prime(sj, strike1, inte, taux, date, vol, strat, K2=strike2, K3=strike3)
+	elif strike2:
+		prime = choose_prime(sj, strike1, inte, taux, date, vol, strat, K2=strike2)
+	else:
+		prime = choose_prime(sj, strike1, inte, taux, date, vol, strat)
+		primes = curve_prime(strike1, inte, taux, date, vol, inf, sup)
+	values = [{'sj': 0, 'vi': 0} for i in range(sup - inf + 1)]
+	f = open("static/strat/csv.csv", "w")
+	g = open("static/strat/primes.csv", "w")
+	for i in range(len(primes)):
+		s = str(primes[i]["sj"]) + ";" + str(primes[i]["prime"]) + "\n"
+		g.write(s)
+	g.close()
+	for i in range(sup - inf + 1):
+		values[i]['sj'] = inf + i
+		if strat == "call":
+			values[i]['vi'] = nb * taille * (max(0, inf+i - strike1) - prime[0]) // 1
+		elif strat == "put":
+			values[i]['vi'] = nb * taille * (max(0, strike1 - (inf+i)) - prime[1]) // 1
+		elif strat == "straddle":
+			values[i]['vi'] = nb * taille * ((max(0, inf+i - strike1) - prime[0]) + (max(0, strike1 - (inf+i)) - prime[1])) // 1
+		elif strat == "strangle":
+			values[i]['vi'] = nb * taille * ((max(0, inf+i - strike2) - prime[0]) + (max(0, strike1 - (inf+i)) - prime[1])) // 1
+		elif strat == "butterfly":
+			values[i]['vi'] = nb * taille * ((max(0, inf+i - strike1) - prime[0]) + (max(0, inf+i - strike3) - prime[2]) - 2 * (max(0, inf+i - strike2) - prime[1])) // 1
+		else:
+			values[i]['vi'] = nb * taille * ((max(0, strike1 - (inf+i)) - prime[0]) + (max(0, inf+i - strike4) - prime[3]) - (max(0, strike2 - (inf+i)) - prime[1]) - (max(0, inf+i - strike3) - prime[2])) // 1
+		if sens == "vente":
+			values[i]['vi'] = -values[i]['vi']
+		s = str(values[i]['sj']) + ";" + str(values[i]['vi']) + "\n"
+		if f:
+			f.write(s)
+	f.close()
+	os.system("../.venv/bin/python3 plot.py strat "+strat+" "+sens)
+	return render_template("strat.html", prime=prime)
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=8000)
--- src/plot.py
+++ src/plot.py
@@ -1,12 +1,18 @@
 #! /usr/bin/python3
 
 import matplotlib.pyplot as plt
+import sys
 
-def plot(opt_type, values):
+def plot(opt_type, values, st_type="", st_sens="", primes=[]):
     payoff = [values[i][opt_type] for i in range(len(values))]
     S0 = [values[i]['prix'] for i in range(len(values))]
+    px = [primes[i]['prix'] for i in range(len(primes))]
+    py = [primes[i]['prime'] for i in range(len(primes))]
     plt.figure(figsize=(20,10))
+    plt.plot(px, py, label='Prime')
     plt.plot(S0, payoff, label='Payoff')
+    if opt_type == "put" or (st_sens == "achat" and st_type in ["put", "straddle", "stangle"]) or (st_sens == "vente" and st_type not in ["put", "straddle", "strangle"]):
+        plt.gca().invert_yaxis()
     plt.xlabel("Sous-jacent")
     plt.ylabel("Payoff")
     plt.title("Payoff de l'option")
@@ -26,4 +32,28 @@ def generate_values():
     plot('call', values)
     plot('put', values)
 
-generate_values()
+def generate_values_strat(st_type, st_sens):
+    f = open("static/strat/csv.csv", 'r')
+    lines = f.readlines()
+    n = len(lines)
+    values = [{ 'prix': 0, 'vi': 0 } for i in range(n)]
+    for i in range(n):
+        L = lines[i].split("\n")[0].split(";")
+        values[i]['prix'] = L[0]
+        values[i]['vi'] = L[1]
+    f.close()
+    g = open("static/strat/primes.csv")
+    lines = g.readlines()
+    n = len(lines)
+    primes = [{ 'prix': 0, 'prime': 0} for i in range(n)]
+    for i in range(n):
+        L = lines[i].split("\n")[0].split(";")
+        primes[i]['prix'] = L[0]
+        primes[i]['prime'] = L[1]
+    g.close()
+    plot('vi', values, st_type, st_sens, primes=primes)
+
+if len(sys.argv) == 4 and sys.argv[1] == "strat":
+    generate_values_strat(sys.argv[2], sys.argv[3])
+else:
+    generate_values()
